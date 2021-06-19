ffab8d30c6af76bdf12bdce64eb803ff4383adb5
proyectoalfaro3cv50 <proyectoalfaro3cv50@gmail.com>
2021-06-18 22:29:49 -0500

    actualizacion

---
+++ js/init.js
@@ -0,0 +1,17 @@
+/*  Inicializa Firebase con la
+ * configuración del proyecto.
+ * Revisa la configuración en tu
+ * servidor de Firebase y cópiala
+ * aquí sustituyendo los
+ * asteriscos. Los campos deben
+ * quedar igual que en tu
+ * servidor. */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyB2IrJ3A1c79ES84uglkXpYRa-MavmiasE",
+    authDomain: "proyectoalfaro3cv500.firebaseapp.com",
+    projectId: "proyectoalfaro3cv500",
+    storageBucket: "proyectoalfaro3cv500.appspot.com",
+    messagingSenderId: "847202787655",
+    appId: "1:847202787655:web:d5bb81ba0525992355ac62"
+});
